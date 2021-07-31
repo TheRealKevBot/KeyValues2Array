51c2d5e848fa56667c291f80fd5d4115325fa0c1
TheRealKevBot <Kglas001@gmail.com>
2021-07-30 21:04:28 -0600

    First

---
+++ index.js
@@ -0,0 +1,15 @@
+function objectToArray(obj) {
+	
+} 
+
+console.log(objectToArray({
+    D: 1, 
+    B: 2, 
+    C: 3
+}), [["D", 1], ["B", 2], ["C", 3]])
+
+console.log(objectToArray({
+	likes: 2, 
+    dislikes: 3, 
+    followers: 10
+}), [["likes", 2], ["dislikes", 3], ["followers", 10]])
